fa2208ccaa325c4ad529b3eb92d71e39a117cb19
jamesurobertson <jamesurobertson@gmail.com>
2022-05-07 14:12:15 -0600

    initial app working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
+const player = "Urquhart";
 const App = () => {
-  return <div></div>;
+  const [accuracy, setAccuracy] = useState(null);
+  useEffect(() => {
+    fetch(`https://lichess.org/api/user/${player}/current-game`, {
+      headers: { Accept: "application/json" },
+    })
+      .then((res) => res.json())
+      .then((d) => {
+        console.log(d);
+        const acl = 23;
+        const accuracyRating = (103.3979 - 0.3820659 * acl - 0.002169231 * acl) ^ 2;
+        setAccuracy(accuracyRating);
+      });
+  }, []);
+  return <div>{accuracy}</div>;
 };
 
 export default App;
